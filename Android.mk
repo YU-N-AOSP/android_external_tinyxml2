3e543e41ba2d91ca3122f85ca8a8d7dfa724b1d8
Alex Sarraf <asarraf@codeaurora.org>
2015-09-24 11:51:25 -0700

    tinyxml2: enable tinyxml2 shared library generation
    This allows user space components to link to this library dynamically.
    
    Change-Id: I7c14dc57f0c60c7a1a207ed84e07551fe61d0973

--- Android.mk
+++ Android.mk
@@ -22,3 +22,16 @@ LOCAL_MODULE:=libtinyxml2
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# shared library
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:=libtinyxml2
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libtinyxml2
+ifeq ($(LOG_TO_ANDROID_LOGCAT),true)
+LOCAL_SHARED_LIBRARIES+= libcutils
+endif
+
+include $(BUILD_SHARED_LIBRARY)
